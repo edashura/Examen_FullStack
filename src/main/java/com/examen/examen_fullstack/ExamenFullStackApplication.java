3391f1133d2470d5cc728264dff2a8ea2c76af24
edashura <80004702+edashura@users.noreply.github.com>
2023-12-08 09:17:14 +0100

    Creating the project

---
+++ src/main/java/com/examen/examen_fullstack/ExamenFullStackApplication.java
@@ -0,0 +1,13 @@
+package com.examen.examen_fullstack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamenFullStackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExamenFullStackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/examen/examen_fullstack/ExamenFullStackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.examen.examen_fullstack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamenFullStackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
